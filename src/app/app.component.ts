8f26ada3de5e74956eaa77b61fb159995952d3e5
kidoGit <pkparekh95@gmail.com>
2020-07-24 21:07:24 +0530

    Add service and fix node-sass issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Item } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-demo';
+  items: Array<Item>;
+  constructor(private apiService: ApiService) {
+  }
+  ngOnInit() {
+    this.fetchData();
+  }
+  fetchData() {
+    this.apiService.fetch().subscribe((data: Array<Item>) => {
+      console.log(data);
+      this.items = data;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
